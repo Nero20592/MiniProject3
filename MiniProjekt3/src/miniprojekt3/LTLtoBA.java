c3ba767b706c53abfa76e5c6d192c46e72637f31
Raphael <Raphael@Raphael-PC.Speedport_W_724V_Typ_A_05011602_00_001>
2015-01-15 11:52:26 +0100

    action wird jetzt aus mehreren IGraphProposition zusammengesetzt

--- MiniProjekt3/src/miniprojekt3/LTLtoBA.java
+++ MiniProjekt3/src/miniprojekt3/LTLtoBA.java
@@ -11,122 +11,125 @@ import rwth.i2.ltl2ba4j.model.ITransition;
 
 public class LTLtoBA {
 
-	
-	 Set<BAState> initialStates = new HashSet<BAState>();
-	 Set<BAState> bAStates = new HashSet<BAState>();
-	 Set<BAState> acceptingStates = new HashSet<BAState>();
-	 Set<BATransition> transitions = new HashSet<BATransition>();
-	 Set<Action> alphabet = new HashSet<Action>();
-	
-	 String[] tempInitialStates;
-	 String[] tempBAStates;
-	 String[] tempAcceptingStates;
-	 String[] tempTransitions;
-	 String[] tempAlphabet;
+	Set<BAState> initialStates = new HashSet<BAState>();
+	Set<BAState> bAStates = new HashSet<BAState>();
+	Set<BAState> acceptingStates = new HashSet<BAState>();
+	Set<BATransition> transitions = new HashSet<BATransition>();
+	Set<Action> alphabet = new HashSet<Action>();
 
-	public LTLtoBA(){
-		
+	String[] tempInitialStates;
+	String[] tempBAStates;
+	String[] tempAcceptingStates;
+	String[] tempTransitions;
+	String[] tempAlphabet;
+
+	public LTLtoBA() {
+
 	}
-	public BA transformLTLtoBA(String value){
-		String temp = value.substring(24, value.length()-2); 
+
+	public BA transformLTLtoBA(String value) {
+		String temp = value.substring(24, value.length() - 2);
 		tempInitialStates = temp.split("shape=box");
 		tempBAStates = value.split("s");
 		tempAcceptingStates = value.split("style=dotted");
 		tempTransitions = value.split("label=");
-		for(String s : tempInitialStates){
+		for (String s : tempInitialStates) {
 			System.out.println("INITIAL: " + s);
 		}
-		for(String a : tempBAStates){
+		for (String a : tempBAStates) {
 			System.out.println("STATES: " + a);
 		}
-		for(String b : tempAcceptingStates){
+		for (String b : tempAcceptingStates) {
 			System.out.println("ACCEPT: " + b);
 		}
-		for(String c : tempTransitions){
+		for (String c : tempTransitions) {
 			System.out.println("TRANS: " + c);
 		}
-		
-		
-		//return new BA(BAstates, initialStates, acceptingStates, transitions, alphabet);
+
+		// return new BA(BAstates, initialStates, acceptingStates, transitions,
+		// alphabet);
 		return null;
-		
-		
+
 	}
-	
-	public BA transformLTLtoBA(Collection<ITransition> automaton){
-		   Set<IState> SourceState = new HashSet<IState>();
-		   Set<IState> TargetState = new HashSet<IState>();
-	        for(ITransition t: automaton) {
-	            IState ISourceState = t.getSourceState();
-	            if(!SourceState.contains(ISourceState)) {
-	                SourceState.add(ISourceState);
-	            }
-	            IState ITargetState = t.getTargetState();
-	            if(!TargetState.contains(ITargetState)) {
-	                TargetState.add(ISourceState);
-	            }
-	        }
-	            
-	            for(IState state: SourceState) {
-	              //   boolean isInitial = false, isFinal = false;
-	                if(state.isInitial()) {
-	                    initialStates.add((BAState) state);
-	                }
-	                if(state.isFinal()) {
-	                    acceptingStates.add((BAState) state);
-	                }
-	            }
-	            
-//	            for(ITransition transition: automaton) {
-//	                if(SourceState.equals(transition)){
-//	                	transitions.add((BATransition) transition);
-//	                	alphabet.add((Action) transition.getLabels());
-//	                }
-//	                
-//	            }
-	            
-		return new BA(bAStates, initialStates, acceptingStates, transitions, alphabet);
-		
+
+	public BA transformLTLtoBA(Collection<ITransition> automaton) {
+		Set<IState> SourceState = new HashSet<IState>();
+		Set<IState> TargetState = new HashSet<IState>();
+		for (ITransition t : automaton) {
+			IState ISourceState = t.getSourceState();
+			if (!SourceState.contains(ISourceState)) {
+				SourceState.add(ISourceState);
+			}
+			IState ITargetState = t.getTargetState();
+			if (!TargetState.contains(ITargetState)) {
+				TargetState.add(ISourceState);
+			}
+		}
+
+		for (IState state : SourceState) {
+			// boolean isInitial = false, isFinal = false;
+			if (state.isInitial()) {
+				initialStates.add((BAState) state);
+			}
+			if (state.isFinal()) {
+				acceptingStates.add((BAState) state);
+			}
+		}
+
+		// for(ITransition transition: automaton) {
+		// if(SourceState.equals(transition)){
+		// transitions.add((BATransition) transition);
+		// alphabet.add((Action) transition.getLabels());
+		// }
+		//
+		// }
+
+		return new BA(bAStates, initialStates, acceptingStates, transitions,
+				alphabet);
+
 	}
-	
-	public static BA ltl2BA(Collection<ITransition> automaton){
+
+	public static BA ltl2BA(Collection<ITransition> automaton) {
 		Set<BAState> states = new HashSet<BAState>();
 		Set<BAState> initStates = new HashSet<BAState>();
 		Set<BAState> accStates = new HashSet<BAState>();
 		Set<BATransition> trans = new HashSet<BATransition>();
 		Set<Action> alphab = new HashSet<Action>();
-		
+
 		for (ITransition t : automaton) {
 			IState source = t.getSourceState();
 			IState target = t.getTargetState();
 			Set<IGraphProposition> prop = t.getLabels();
-			BAState begin = new BAState(source.getLabel(), source.isInitial(), source.isFinal());
-			BAState end = new BAState(target.getLabel(), target.isInitial(), target.isFinal());
-			Action action = new Action(prop.iterator().next().getFullLabel());
+			BAState begin = new BAState(source.getLabel(), source.isInitial(),
+					source.isFinal());
+			BAState end = new BAState(target.getLabel(), target.isInitial(),
+					target.isFinal());
+			String actionString = "";
+			for (IGraphProposition iGraphProposition : prop) {
+				actionString = actionString + iGraphProposition.getLabel()
+						+ ",";
+			}
+			Action action = new Action(actionString.substring(0,
+					actionString.length() - 1));
 			BATransition transition = new BATransition(begin, end, action);
 			states.add(begin);
 			states.add(end);
-			if(begin.isInitial()){
+			if (begin.isInitial()) {
 				initStates.add(begin);
 			}
-			if(end.isInitial()){
+			if (end.isInitial()) {
 				initStates.add(end);
 			}
-			if(begin.isFinal()){
+			if (begin.isFinal()) {
 				accStates.add(begin);
 			}
-			if(end.isFinal()){
+			if (end.isFinal()) {
 				accStates.add(end);
 			}
 			alphab.add(action);
 			trans.add(transition);
 		}
-		System.out.println(accStates);
 		return new BA(states, initStates, accStates, trans, alphab);
 	}
-	
-	
-	
-	
-	
+
 }
